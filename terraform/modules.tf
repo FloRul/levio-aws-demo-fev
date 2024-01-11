22925f1280f6fa9f82f435ca354b42ca4fa84e16
FloRul <rumiel.florian@gmail.com>
2024-01-11 17:38:07 -0500

    Fix lambda_vpc_security_group_ids in ingestion module

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -2,7 +2,7 @@ module "ingestion" {
   source              = "../ingestion"
   storage_bucket_name = "levio-demo-fev-storage"
   lambda_vpc_security_group_ids = [
-    aws_security_group.lambda_ingestion_sg,
+    aws_security_group.lambda_ingestion_sg.id,
   ]
   lambda_vpc_subnet_ids          = module.vpc.public_subnets
   pg_vector_host                 = aws_db_instance.vector_db.address
