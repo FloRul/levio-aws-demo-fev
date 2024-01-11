4d666baa8e13c5987f9ec04712275bf26933ca8a
FloRul <rumiel.florian@gmail.com>
2024-01-11 16:30:06 -0500

    Update ingestion deployment workflow and code

--- ingestion/lambda.tf
+++ ingestion/lambda.tf
@@ -1,4 +1,5 @@
 module "lambda_function_container_image" {
+  timeout                  = 20
   source                   = "terraform-aws-modules/lambda/aws"
   function_name            = var.lambda_function_name
   create_package           = false
