8e2cfd98ad9b00783740e4911e908601c80461d2
FloRul <rumiel.florian@gmail.com>
2024-01-12 11:22:41 -0500

    Remove unnecessary SQS permissions from lambda_policy.tf

--- ingestion/lambda_policy.tf
+++ ingestion/lambda_policy.tf
@@ -75,14 +75,4 @@ data "aws_iam_policy_document" "lambda_policy_doc" {
       var.secret_arn
     ]
   }
-  statement {
-    actions = [
-      "sqs:ReceiveMessage",
-      "sqs:DeleteMessage",
-      "sqs:GetQueueUrl",
-    ]
-    resources = [
-      aws
-    ]
-  }
 }
