86c9675758b8f3d9843eacaaf1a42ef92f153a1a
FloRul <rumiel.florian@gmail.com>
2024-01-15 12:10:35 -0500

    Update event record parsing in lambda_handler function

--- ingestion/src/index.py
+++ ingestion/src/index.py
@@ -95,7 +95,7 @@ def get_bucket_and_key(record):
 
 def lambda_handler(event, context):
     print(event)
-    records = event['Records']
+    records = json.loads(event['Records'][0]['body'])['Records']
     for record in records:
         eventName = record['eventName']
         logging.info(f"eventName: {eventName}")
