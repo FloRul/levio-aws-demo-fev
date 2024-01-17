32b9858bfdf99ea307d9dae59083f2a04862c842
FloRul <rumiel.florian@gmail.com>
2024-01-17 15:31:37 -0500

    Update VPC endpoint configuration

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -26,11 +26,10 @@ resource "aws_vpc_endpoint" "s3_endpoint" {
 }
 
 resource "aws_vpc_endpoint" "dynamo_db_endpoint" {
-  vpc_id             = module.vpc.vpc_id
-  service_name       = "com.amazonaws.${var.aws_region}.dynamodb"
-  vpc_endpoint_type  = "Gateway"
-  security_group_ids = [aws_security_group.dynamo_db_sg.id]
-  subnet_ids         = module.vpc.public_subnets
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.${var.aws_region}.dynamodb"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = module.vpc.public_route_table_ids
 }
 
 resource "aws_vpc_endpoint" "secrets_manager_endpoint" {
