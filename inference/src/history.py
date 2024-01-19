db4d631d2c41617f7718041be501615dd0d8caa9
FloRul <rumiel.florian@gmail.com>
2024-01-18 20:33:08 -0500

    Refactor response handling in History class

--- inference/src/history.py
+++ inference/src/history.py
@@ -20,8 +20,7 @@ class History:
                 InvocationType="RequestResponse",
                 Payload=json.dumps(payload),
             )
-            print("memory reached")
-            return response["Payload"].read().decode("utf-8")
+            return json.loads(response["Payload"].read().decode("utf-8"))
         except ClientError as e:
             print("Error occurred: ", e.response["Error"]["Message"])
             return e.response["Error"]["Message"]
