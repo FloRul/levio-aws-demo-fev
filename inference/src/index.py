3bb10f2e70202bf404864f3f5c896dddf1139513
FloRul <rumiel.florian@gmail.com>
2024-01-18 20:46:06 -0500

    Refactor history.get() method and add debug print statements

--- inference/src/index.py
+++ inference/src/index.py
@@ -123,7 +123,7 @@ def lambda_handler(event, context):
 
                 if enable_history == 1:
                     chat_history = history.get(limit=10)
-
+                    print(f"chat_history :{chat_history}")
                 # prepare the prompt
                 prompt = prepare_prompt(query, docs, chat_history)
                 print(f"prompt :{prompt}")
